b4725bb9cc22c42b1404a8bde118c84e65f08b72
carol-zilker <carolinezilker@gmail.com>
2023-05-08 18:12:19 -0300

    v1

--- simulado_jazz.py
+++ simulado_jazz.py
@@ -109,7 +109,7 @@ html_br="""
     """
 
 with st.container():
-        col1, col2, col3 = st.columns([5,5,5])
+        col1, col2, col3 = st.columns([3,5,3])
         with col1:
             st.markdown(html_br, unsafe_allow_html=True)
         with col2:
