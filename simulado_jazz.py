6565625ba337f2f92aba7a8b07a2650d360a677f
carol-zilker <carolinezilker@gmail.com>
2023-05-08 18:06:41 -0300

    v1

--- simulado_jazz.py
+++ simulado_jazz.py
@@ -110,7 +110,7 @@ html_br="""
 
 with st.container():
         col1, col2, col3 = st.columns([2, 10, 2])
-         with col1:
+        with col1:
             st.markdown(html_br, unsafe_allow_html=True)
         with col2:
             st.image('[LOGO 3] Jazz.png')
