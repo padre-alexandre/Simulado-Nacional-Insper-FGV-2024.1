aa4dec1ea8ef1d30d9a96406fc2ce7a48da2c47f
carol-zilker <carolinezilker@gmail.com>
2023-05-16 19:45:45 -0300

    v2

--- simulado_jazz.py
+++ simulado_jazz.py
@@ -254,8 +254,8 @@ if login_aluno != '':
     
     resultados_gerais4 = resultados_gerais3[resultados_gerais3['Nota na questão'] > 0]
     resultados_gerais4_aux = resultados_gerais4[['Login do aluno(a)','Número da questão','Tempo na questão','Valor da questão','Unnamed: 10','Acerto','Nota na questão']]
-    st.dataframe(resultados_gerais4_aux)
-    resultados_gerais5 = resultados_gerais4.groupby('Login do aluno(a)').mean().reset_index()
+    resultados_gerais5 = resultados_gerais4_aux.copy()
+    #resultados_gerais5 = resultados_gerais4.groupby('Login do aluno(a)').mean().reset_index()
     st.dataframe(resultados_gerais5)
     alunos_fizeram = pd.DataFrame()
     alunos_fizeram['Nome do aluno(a)'] = resultados_gerais4['Nome do aluno(a)']
